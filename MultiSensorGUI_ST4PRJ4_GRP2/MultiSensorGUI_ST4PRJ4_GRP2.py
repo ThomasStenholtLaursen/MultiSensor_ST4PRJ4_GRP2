f1dcde4f9172c4e8f3ebc0f017673442a7d7a4df
ThomasStenholtLaursen <201905236@post.au.dk>
2022-05-04 11:34:56 +0200

    Created buttons - a 'settings' and a 'save' button. Unsure wether we need a 'save' button, but it's added for now

--- MultiSensorGUI_ST4PRJ4_GRP2/MultiSensorGUI_ST4PRJ4_GRP2.py
+++ MultiSensorGUI_ST4PRJ4_GRP2/MultiSensorGUI_ST4PRJ4_GRP2.py
@@ -1,15 +1,38 @@
 from tkinter import *
+import tkinter as tk
 
 
-gui = Tk()
-gui.geometry("800x480")
-gui.title('Humidity GUI')
+root = tk.Tk()
 
-bg = PhotoImage(file = "template_bg1.png")
-bg_label = Label(gui, image=bg)
-bg_label.place(x=0,y=0, relwidth=1, relheight=1)
+HEIGHT = 480
+WIDTH = 800
+root.minsize(WIDTH, HEIGHT)
+root.maxsize(WIDTH, HEIGHT)
+
+background_image = tk.PhotoImage(file='template_bg2.png')
+background_label = tk.Label(root, image=background_image)
+background_label.place(relwidth=1, relheight=1)
 
 
+frame = tk.Frame(root)
+frame.configure(bg='#424242')
+frame.place(relwidth=1, y=425, height=55)
 
 
-gui.mainloop()
+button_one = tk.Button(frame, text="Save",
+                      font=20, bd='0', width=20, height=30, 
+                       bg='#08b4b5', fg='#ffffff', 
+                       activebackground='#077e7f', 
+                       activeforeground='#ffffff')
+button_one.pack(side=tk.LEFT, expand=True)
+
+
+button_two = tk.Button(frame, text="Settings",
+                      font=20, bd='0', width=20, height=30, 
+                       bg='#08b4b5', fg='#ffffff', 
+                       activebackground='#077e7f', 
+                       activeforeground='#ffffff')
+button_two.pack(side=tk.RIGHT, expand=True)
+
+
+root.mainloop()
