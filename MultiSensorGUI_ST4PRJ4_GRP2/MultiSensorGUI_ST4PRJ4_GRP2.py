d0e66bcbb48d888ec7e4a9b77d4e5f1be56c5095
ThomasStenholtLaursen <201905236@post.au.dk>
2022-05-06 14:05:17 +0200

    Added Observer Pattern for each changeing object (indicators, light and temp). Tested with random generated numbers. Not implementing Producer/Consumer pattern for now as there'll be a possible time-delay.

--- MultiSensorGUI_ST4PRJ4_GRP2/MultiSensorGUI_ST4PRJ4_GRP2.py
+++ MultiSensorGUI_ST4PRJ4_GRP2/MultiSensorGUI_ST4PRJ4_GRP2.py
@@ -3,6 +3,31 @@ import tkinter as tk
 import ObserverPattern as ob
 import threading as t
 
+GREEN = '#00ff30'
+
+root = tk.Tk()
+
+#Test functions for observer pattern
+#region
+def obtest1():
+    ob.subject_left.some_business_logic()
+
+def obtest2():
+    ob.subject_right.some_business_logic()
+
+def obtest3():
+    ob.subject_top.some_business_logic()
+
+def obtest4():
+    ob.subject_bottom.some_business_logic()
+
+def obtest5():
+    ob.subject_temp.some_business_logic()
+
+def obtest6():
+    ob.subject_light.some_business_logic()
+#endregion
+
 #Color functions
 #region
 def redAll():
@@ -12,42 +37,38 @@ def redAll():
     bottom.configure(bg='red')
 
 def greenAll():
-    left.configure(bg='green')
-    right.configure(bg='green')
-    top.configure(bg='green')
-    bottom.configure(bg='green')
+    left.configure(bg=GREEN)
+    right.configure(bg=GREEN)
+    top.configure(bg=GREEN)
+    bottom.configure(bg=GREEN)
 
 def redRight():
     right.configure(bg='red')
 
 def greenRight():
-    right.configure(bg='green')
+    right.configure(bg=GREEN)
 
 def redLeft():
     left.configure(bg='red')
 
 def greenLeft():
-    left.configure(bg='green')
+    left.configure(bg=GREEN)
 
 def redTop():
     top.configure(bg='red')
 
 def greenTop():
-    top.configure(bg='green')
+    top.configure(bg=GREEN)
 
 def redBottom():
     bottom.configure(bg='red')
 
 def greenBottom():
-    bottom.configure(bg='green')
+    bottom.configure(bg=GREEN)
 #endregion
 
-
-def obtest():
-    ob.subject.some_business_logic()
-    ob.subject.some_business_logic()
-    
-root = tk.Tk()
+#GUI
+#region
 
 #Locking window and matching resolution to touch display (800x480)
 #region
@@ -68,12 +89,18 @@ background_label.place(relwidth=1, relheight=1)
 #region
 left = tk.Canvas(root, width=10, height =10, bg ='green', bd=10)
 left.place(y=180,x=342)
+
 right = tk.Canvas(root, width=10, height =10, bg ='green', bd=10)
 right.place(y=180,x=683)
+
 top = tk.Canvas(root, width=10, height =10, bg ='green', bd=10)
 top.place(y=13,x=513)
+
 bottom = tk.Canvas(root, width=10, height =10, bg ='green', bd=10)
 bottom.place(y=346,x=513)
+
+light = tk.Canvas(root, width=10, height =10, bg ='#00ff30', bd=10)
+light.place(y=125,x=132)
 #endregion
 
 #Creating frame for buttons and creating buttons
@@ -106,70 +133,118 @@ button_two.pack(side=tk.RIGHT, expand=True)
 temp_label = tk.Label(root, font=("Segoe UI", 20), text="70", bg='#777777',fg='red')
 temp_label.place(y=300, x=130)
 
-light_label = tk.Label(root, font=("Segoe UI",20), text="GOOD",bg='#777777',fg='#00ff30')
-light_label.place(y=120, x=110)
+#light_label = tk.Label(root, font=("Segoe UI",20), text="GOOD",bg='#777777',fg='#00ff30')
+#light_label.place(y=120, x=110)
 #endregion
 
-#Creation of Observers
+#endregion    
+
+#Observers
 #region
 class ConcreteObserverLeft(ob.Observer):
     def update(self, subject: ob.Subject) -> None:
-        if subject._state == 1:           
+        if subject._state <= 1:           
             greenLeft()
-            print(subject._state)
+            print("Left Indicator: ", subject._state)
         else:
             redLeft()
-            print(subject._state)
+            print("Left Indicator: ", subject._state)
 
 class ConcreteObserverRight(ob.Observer):
     def update(self, subject: ob.Subject) -> None:
-        if subject._state == 2:           
+        if subject._state <= 2:           
             greenRight()
-            print(subject._state)
+            print("Right Indicator: ", subject._state)
         else:
             redRight()
-            print(subject._state)
+            print("Right Indicator: ", subject._state)
 
 class ConcreteObserverTop(ob.Observer):
     def update(self, subject: ob.Subject) -> None:
-        if subject._state == 3:           
+        if subject._state <= 2:           
             greenTop()
-            print(subject._state)
+            print("Top Indicator: ", subject._state)
         else:
             redTop()
-            print(subject._state)
+            print("Top Indicator: ", subject._state)
 
 class ConcreteObserverBottom(ob.Observer):
     def update(self, subject: ob.Subject) -> None:
-        if subject._state == 4:           
+        if subject._state <= 2:           
             greenBottom()
-            print(subject._state)
+            print("Bottom Indicator: ", subject._state)
         else:
             redBottom()
-            print(subject._state)
+            print("Bottom Indicator: ", subject._state)
+
+class ConcreteObserverTemp(ob.Observer):
+    def update(self, subject: ob.Subject) -> None:
+        temp_label.configure(text=subject._state)
+        if subject._state < 50:
+            temp_label.configure(fg=GREEN)
+            print("Temp Indicator: ", subject._state)
+        else:
+            temp_label.configure(fg='red')
+            print("Temp Indicator: ", subject._state)
+
+class ConcreteObserverLight(ob.Observer):
+    def update(self, subject: ob.Subject) -> None:
+        if subject._state > 25:
+            light.configure(bg=GREEN)
+            print("Light Indicator: ", subject._state)
+        else:
+            light.configure(bg='red')
+            print("Light Indicator: ", subject._state)
+                      
+
+#Attaching observers
+#region
+ob.subject_left = ob.ConcreteSubjectLeft()
+ob.subject_right = ob.ConcreteSubjectRight()
+ob.subject_top = ob.ConcreteSubjectTop()
+ob.subject_bottom = ob.ConcreteSubjectBottom()
+ob.subject_temp = ob.ConcreteSubjectTemp()
+ob.subject_light = ob.ConcreteSubjectLight()
+
+observer_left = ConcreteObserverLeft()
+ob.subject_left.attach(observer_left)
+
+observer_right = ConcreteObserverRight()
+ob.subject_right.attach(observer_right)
+
+observer_top = ConcreteObserverTop()
+ob.subject_top.attach(observer_top)
+
+observer_bottom = ConcreteObserverBottom()
+ob.subject_bottom.attach(observer_bottom)
+
+observer_temp = ConcreteObserverTemp()
+ob.subject_temp.attach(observer_temp)
+
+observer_light = ConcreteObserverLight()
+ob.subject_light.attach(observer_light)
+#endregion
 #endregion
 
+#Threads
+#region
+threadright = t.Thread(target=obtest1)
+threadright.start()
 
-if __name__ == "__main__":
-    # The client code.
+threadleft = t.Thread(target=obtest2)
+threadleft.start()
 
-    ob.subject = ob.ConcreteSubject()
+threadtop = t.Thread(target=obtest3)
+threadtop.start()
 
-    observer_left = ConcreteObserverLeft()
-    ob.subject.attach(observer_left)
+threadbottom = t.Thread(target=obtest4)
+threadbottom.start()
 
-    observer_right = ConcreteObserverRight()
-    ob.subject.attach(observer_right)
+threadtemp = t.Thread(target=obtest5)
+threadtemp.start()
 
-    observer_top = ConcreteObserverTop()
-    ob.subject.attach(observer_top)
-
-    observer_bottom = ConcreteObserverBottom()
-    ob.subject.attach(observer_bottom)
-
-
-threadtest = t.Thread(target=obtest)
-
-threadtest.start()
+threadlight = t.Thread(target=obtest6)
+threadlight.start()
+#endregion
 
 root.mainloop()
--- MultiSensorGUI_ST4PRJ4_GRP2/ObserverPattern.py
+++ MultiSensorGUI_ST4PRJ4_GRP2/ObserverPattern.py
@@ -4,106 +4,154 @@ from random import randrange
 from typing import List
 import time as t
 
+SLEEPBUSINESS = 0.5
+
 
 class Subject(ABC):
-    """
-    The Subject interface declares a set of methods for managing subscribers.
-    """
-
     @abstractmethod
     def attach(self, observer: Observer) -> None:
-        """
-        Attach an observer to the subject.
-        """
         pass
 
     @abstractmethod
-    def detach(self, observer: Observer) -> None:
-        """
-        Detach an observer from the subject.
-        """
+    def detach(self, observer: Observer) -> None:        
         pass
 
     @abstractmethod
-    def notify(self) -> None:
-        """
-        Notify all observers about an event.
-        """
+    def notify(self) -> None:        
         pass
 
-
-class ConcreteSubject(Subject):
-    """
-    The Subject owns some important state and notifies observers when the state
-    changes.
-    """
-
+class ConcreteSubjectLeft(Subject):    
     _state: int = None
-    """
-    For the sake of simplicity, the Subject's state, essential to all
-    subscribers, is stored in this variable.
-    """
-
+    
     _observers: List[Observer] = []
-    """
-    List of subscribers. In real life, the list of subscribers can be stored
-    more comprehensively (categorized by event type, etc.).
-    """
-
-    def attach(self, observer: Observer) -> None:
-        print("Subject: Attached an observer.")
+    
+    def attach(self, observer: Observer) -> None:        
         self._observers.append(observer)
 
     def detach(self, observer: Observer) -> None:
         self._observers.remove(observer)
 
-    """
-    The subscription management methods.
-    """
-
     def notify(self) -> None:
-        """
-        Trigger an update in each subscriber.
-        """
-
-        print("Subject: Notifying observers...")
         for observer in self._observers:
             observer.update(self)
 
-    def some_business_logic(self) -> None:
-        """
-        Usually, the subscription logic is only a fraction of what a Subject can
-        really do. Subjects commonly hold some important business logic, that
-        triggers a notification method whenever something important is about to
-        happen (or after it).
-        """
-        for x in range(50):
-            print("\nSubject: I'm doing something important.")
-            self._state = randrange(0, 4)
-            print(f"Subject: My state has just changed to: {self._state}")
+    def some_business_logic(self) -> None:        
+        for x in range(50):            
+            self._state = randrange(0, 4)            
             self.notify()
-            t.sleep(1)
+            t.sleep(SLEEPBUSINESS)
 
+class ConcreteSubjectRight(Subject):
+   
+    _state: int = None
+    
+    _observers: List[Observer] = []
+    
+    def attach(self, observer: Observer) -> None:       
+        self._observers.append(observer)
+
+    def detach(self, observer: Observer) -> None:
+        self._observers.remove(observer)
+
+    def notify(self) -> None:
+        for observer in self._observers:
+            observer.update(self)
+
+    def some_business_logic(self) -> None:        
+        for x in range(50):            
+            self._state = randrange(0, 4)            
+            self.notify()
+            t.sleep(SLEEPBUSINESS)
+        
+class ConcreteSubjectTop(Subject):
+    
+    _state: int = None
+    
+    _observers: List[Observer] = []
+    
+    def attach(self, observer: Observer) -> None:        
+        self._observers.append(observer)
+
+    def detach(self, observer: Observer) -> None:
+        self._observers.remove(observer)
+
+    def notify(self) -> None:
+        for observer in self._observers:
+            observer.update(self)
+
+    def some_business_logic(self) -> None:       
+        for x in range(50):
+            self._state = randrange(0, 4)
+            self.notify()
+            t.sleep(SLEEPBUSINESS)
+
+class ConcreteSubjectBottom(Subject):
+    
+    _state: int = None
+    
+    _observers: List[Observer] = []
+    
+    def attach(self, observer: Observer) -> None:
+        self._observers.append(observer)
+
+    def detach(self, observer: Observer) -> None:
+        self._observers.remove(observer)
+
+    def notify(self) -> None:
+        for observer in self._observers:
+            observer.update(self)
+
+    def some_business_logic(self) -> None:        
+        for x in range(50):
+            self._state = randrange(0, 4)
+            self.notify()
+            t.sleep(SLEEPBUSINESS)
+
+class ConcreteSubjectTemp(Subject):
+    
+    _state: int = None
+    
+    _observers: List[Observer] = []
+    
+    def attach(self, observer: Observer) -> None:
+        self._observers.append(observer)
+
+    def detach(self, observer: Observer) -> None:
+        self._observers.remove(observer)
+
+    def notify(self) -> None:        
+        for observer in self._observers:
+            observer.update(self)
+
+    def some_business_logic(self) -> None:        
+        for x in range(50):
+            self._state = randrange(10, 100)
+            self.notify()
+            t.sleep(SLEEPBUSINESS)
+
+class ConcreteSubjectLight(Subject):
+    
+    _state: int = None
+    
+    _observers: List[Observer] = []
+    
+    def attach(self, observer: Observer) -> None:
+        self._observers.append(observer)
+
+    def detach(self, observer: Observer) -> None:
+        self._observers.remove(observer)
+
+    def notify(self) -> None:
+        for observer in self._observers:
+            observer.update(self)
+
+    def some_business_logic(self) -> None:        
+        for x in range(50):
+            self._state = randrange(0, 50)
+            self.notify()
+            t.sleep(SLEEPBUSINESS)
 
 class Observer(ABC):
-    """
-    The Observer interface declares the update method, used by subjects.
-    """
-
     @abstractmethod
-    def update(self, subject: Subject) -> None:
-        """
-        Receive update from subject.
-        """
+    def update(self, subject: Subject) -> None:        
         pass
-
-
-"""
-Concrete Observers react to the updates issued by the Subject they had been
-attached to.
-"""
-
-
-
-
-
