2e9881091616d51480b3cb84210a42c28a02f6bc
Samuel Lund Jessen <au646967@uni.au.dk>
2022-05-19 09:39:13 +0200

    Made threads daemon and fixed, so that both gui and producer/consumer runs at the same time.

--- MultiSensorGUI_ST4PRJ4_GRP2/MultiSensorGUI_ST4PRJ4_GRP2.py
+++ MultiSensorGUI_ST4PRJ4_GRP2/MultiSensorGUI_ST4PRJ4_GRP2.py
@@ -9,27 +9,34 @@ from threading import Thread
 #fullscreen for RPI - next line needs to be included when running with display on RPI
 #root.attributes('-fullscreen', True)
 
-    
+
+
+
+
+
 if __name__ == "__main__":
     max = 1000
     work = Queue()
     finished = Queue()
 
+    
+
     producer = Thread(target=p.run,args=[work,finished,max])
     consumer = Thread(target=c.run,args=[work,finished])
+    producer.daemon = True
+    consumer.daemon = True
 
-    producer.start()
-    consumer.start()
-
-    producer.join()
-    print('Producer has finished')
-
-    consumer.join()
-    print('Consumer has finished')
-
-    print('Finished')
-
-
+    def runSensorThreads():
+        producer.start()
+        consumer.start()
+    
 
     app = MainWindow()
+
+    app.after(0, runSensorThreads)
+    
+    
     app.mainloop()
+
+    
+    
