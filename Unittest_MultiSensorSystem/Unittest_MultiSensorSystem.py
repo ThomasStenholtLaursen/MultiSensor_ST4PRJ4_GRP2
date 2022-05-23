a5b1f1060d3fc134dd12c900a03cb36ebbbf57c2
Julie Thomsen <78859162+juliestorgaardthomsen1998@users.noreply.github.com>
2022-05-23 10:47:14 +0200

    Test

--- Unittest_MultiSensorSystem/Unittest_MultiSensorSystem.py
+++ Unittest_MultiSensorSystem/Unittest_MultiSensorSystem.py
@@ -2,7 +2,7 @@ import unittest
 
 class TestLightTempSensor(unittest.TestCase):
     def test_Temp(self):
-        self.fail("Not implemented")
+        self.fail("hej")
 
     def test_Light(self):
         self.fail("Not implemented")
