0137393070393b1c18b0d9b021fd4a69f494486a
Julie Thomsen <78859162+juliestorgaardthomsen1998@users.noreply.github.com>
2022-05-23 10:43:50 +0200

    Added classes to unittest

--- Unittest_MultiSensorSystem/Unittest_MultiSensorSystem.py
+++ Unittest_MultiSensorSystem/Unittest_MultiSensorSystem.py
@@ -1,2 +1,11 @@
 import unittest
 
+class TestLightTempSensor(unittest.TestCase):
+    def test_Temp(self):
+        self.fail("Not implemented")
+
+    def test_Light(self):
+        self.fail("Not implemented")
+
+if __name__ == '__main__':
+    unittest.main()
