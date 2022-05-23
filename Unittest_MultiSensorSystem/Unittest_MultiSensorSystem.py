af3e68cde40f5a4472ed537409126311dbde2460
Julie Thomsen <78859162+juliestorgaardthomsen1998@users.noreply.github.com>
2022-05-23 11:27:32 +0200

    nothing important happen

--- Unittest_MultiSensorSystem/Unittest_MultiSensorSystem.py
+++ Unittest_MultiSensorSystem/Unittest_MultiSensorSystem.py
@@ -1,11 +1,11 @@
 import unittest
+import DataInput
 
 class TestLightTempSensor(unittest.TestCase):
     def test_Temp(self):
-        self.fail("hej")
+        
 
     def test_Light(self):
-        self.fail("Not implemented")
 
 if __name__ == '__main__':
     unittest.main()
