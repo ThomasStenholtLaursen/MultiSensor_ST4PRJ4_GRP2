a51a5516f46b6881cf1ed34113d31002a2dd5998
Torbjørg <torbjorg-hentze@hotmail.com>
2022-05-23 11:12:27 +0200

    testet

--- Unittest_MultiSensorSystem/Unittest_MultiSensorSystem.py
+++ Unittest_MultiSensorSystem/Unittest_MultiSensorSystem.py
@@ -1,4 +1,6 @@
+from typing import Union
 import unittest
+
 
 class TestLightTempSensor(unittest.TestCase):
     def test_Temp(self):
@@ -6,6 +8,13 @@ class TestLightTempSensor(unittest.TestCase):
 
     def test_Light(self):
         self.fail("Not implemented")
+    def test_threshold(self)
+        
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
+
+        
